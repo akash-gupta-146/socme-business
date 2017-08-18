b386b6ef0bf6e45b4025f8f68341e3b29632c179
akash-gupta-146 <ag.akash007@gmail.com>
2017-08-18 14:20:34 +0530

    added new page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { IndividualPage } from '../pages/individual/individual';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -13,7 +14,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   declarations: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    IndividualPage
   ],
   imports: [
     BrowserModule,
@@ -23,7 +25,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    IndividualPage
   ],
   providers: [
     StatusBar,
---
+++ src/pages/individual/individual.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'individual',
+  templateUrl: 'individual.html'
+})
+export class IndividualPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
