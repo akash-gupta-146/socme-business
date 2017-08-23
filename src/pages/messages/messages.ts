ee1e58246dfcd62f3a13bdac617c2a1d60c576f2
nidhi5 <90nidhikapoor@gmail.com>
2017-08-23 17:18:30 +0530

    Added messages and social managers

--- src/pages/messages/messages.ts
+++ src/pages/messages/messages.ts
@@ -3,7 +3,51 @@ import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'messages',
-  templateUrl: 'messages.html'
+  templateUrl: 'messages.html',
+  styles: [`
+
+    ion-content {
+      background-color: #b2dff4 !important;
+    }
+    .div_social span {
+      display: inline-block;
+      width: 40px;
+      height: 40px;
+      border-radius: 50%;
+      background-size: contain;
+      margin: 10px 14px;
+      position: relative;
+    }
+    .div_social span:nth-child(1) {
+      background-image: url(/assets/icon/4hangouts.png);
+      color: #4c9c3e;
+    }
+    .div_social span:nth-child(2) {
+      background-image: url(/assets/icon/2skype.png);
+      color: #24aded;
+    }
+    .div_social span:nth-child(3) {
+      background-image: url(/assets/icon/3whatsapp.png);
+      color: #22951e;
+    }
+    .div_social span:nth-child(4) {
+      background-image: url(/assets/icon/messenger.png);
+      color: #157dfc;
+    }
+    .div_social span:nth-child(5) {
+      background-image: url(/assets/icon/1viber.png);
+      color: #73509a;
+    }
+
+    .div_social span h6 {
+      font-size: 12px;
+      position: absolute;
+      top: 90%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+    }
+
+  `]
 })
 export class MessagesPage {
 
