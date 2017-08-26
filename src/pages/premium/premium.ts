980ed5791529fee6547f4ea788e8b07bab1be739
akash-gupta-146 <ag.akash007@gmail.com>
2017-08-26 09:57:33 +0530

    updated

--- src/pages/premium/premium.ts
+++ src/pages/premium/premium.ts
@@ -3,7 +3,21 @@ import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'premium',
-  templateUrl: 'premium.html'
+  templateUrl: 'premium.html',
+  styles: [`
+  .sales-header{
+    background:#756ddd;
+    color:white;
+  }
+  .business-header{
+    background:#0091ca;
+    color:white;
+  }
+  .career-header{
+    background:#2f7b15;
+    color:white;
+  }
+  `]
 })
 export class PremiumPage {
 
