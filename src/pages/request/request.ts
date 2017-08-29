196e6f318980ac40a9a5f1624013201418c63159
Nidhi <90nidhikapoor@gmail.com>
2017-08-29 16:28:45 +0530

    REQUEST UPDATED

--- src/pages/request/request.ts
+++ src/pages/request/request.ts
@@ -3,7 +3,16 @@ import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'request',
-  templateUrl: 'request.html'
+  templateUrl: 'request.html',
+  styles: [`
+    .apply_color {
+      background-color: #b2dff4 !important;
+    }
+
+    .swiper_color {
+  color: #007aff !important;
+}
+  `]
 })
 export class RequestPage {
 
