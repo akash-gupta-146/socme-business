a77fe732104a2b2d9e24c748d83d24eae149f50c
Nidhi <90nidhikapoor@gmail.com>
2017-08-29 16:45:51 +0530

    PAYMENT HISTORY

--- src/pages/payment-history/payment-history.ts
+++ src/pages/payment-history/payment-history.ts
@@ -11,6 +11,13 @@ import { NavController } from 'ionic-angular';
     ion-card-header{
       padding-bottom: 0px;
     }
+    .apply_color {
+      background-color: #b2dff4 !important;
+    }
+
+    .swiper_color {
+  color: #007aff !important;
+}
   `]
 })
 export class PaymentHistoryPage {
